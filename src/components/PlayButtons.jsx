4e52628639c3980a2cbf81e84375de362edde2fa
Mukund Bhudia <mukundbhudia1234@gmail.com>
2025-11-02 23:42:14 +1030

    Break provided HTML to components

---
+++ src/components/PlayButtons.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const PlayButtons = () => {
+  return (
+    <>
+      <button id="play" className="btn btn-outline-primary">
+        Play
+      </button>
+      <button id="stop" className="btn btn-outline-primary">
+        Stop
+      </button>
+    </>
+  );
+};
+
+export default PlayButtons;
---
+++ src/components/ProcButtons.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ProcButtons = () => {
+  return (
+    <>
+      <button id="process" className="btn btn-outline-primary">
+        Preprocess
+      </button>
+      <button id="process_play" className="btn btn-outline-primary">
+        Proc & Play
+      </button>
+    </>
+  );
+};
+
+export default ProcButtons;
